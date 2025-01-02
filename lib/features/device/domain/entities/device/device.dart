7aa019df4a3be1053da855224e3913fecfba156e
390willgg <gunawanandreas02@gmail.com>
2025-01-02 15:40:49 +0700

    upgrade for device hive field id

--- lib/features/device/domain/entities/device/device.dart
+++ lib/features/device/domain/entities/device/device.dart
@@ -1,24 +1,25 @@
 import 'package:equatable/equatable.dart';
 import 'package:hive/hive.dart';
+
 import '../soil_measurement/soil_measurement.dart';
 
 part 'device.g.dart';
 
 @HiveType(typeId: 4)
 class Device extends Equatable {
-  @HiveField(11)
+  @HiveField(1)
   final String id;
-  @HiveField(12)
-  final List<SoilMeasurement> datas;
+  @HiveField(2)
+  final List<SoilMeasurement> data;
 
   const Device({
     required this.id,
-    required this.datas,
+    required this.data,
   });
 
   @override
   List<Object?> get props => [
         id,
-        datas,
+        data,
       ];
 }
